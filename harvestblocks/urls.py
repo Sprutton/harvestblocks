1040a0a3ca30c731688c57024c9caed4fc375934
Samual Prutton <Samprutton@gmail.com>
2021-06-05 22:56:37 -0700

    adding static paths

--- harvestblocks/urls.py
+++ harvestblocks/urls.py
@@ -17,11 +17,16 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
+from django.conf.urls import url
 
+from django.views.static import serve
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('vendors/', include('apps.vendor.urls')),
     path('cart/', include('apps.cart.urls')),
     path('', include('apps.core.urls')),
-    path('', include('apps.product.urls'))
+    path('', include('apps.product.urls')),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
